7b244942bc590ca35f25da0994423bbf53037b07
Eva Jeliazkova <eva@wustl.edu>
2026-02-18 17:08:41 -0600

    restore bunny movement stash

--- Assets/Code/GamePlay/Enemies/RunTime/Bunny.cs
+++ Assets/Code/GamePlay/Enemies/RunTime/Bunny.cs
@@ -6,13 +6,56 @@ using UnityEditor.Animations;
 public class Bunny : Enemy {
     private bool isAngry = false;
 
+    // Hopping
+    private float hopDuration = 0.3f;
+    private float landDuration = 0.15f;
+    private float hopHeight = 0.5f;
+
+    // Hopping state management
+    private float hopTimer = 0f;
+    private bool isHopping = true;
+    private Vector3 groundPos;
+    private bool initialized = false;
+
     protected override void Move() {
-        Vector3 direction = (target - transform.position).normalized;
+        if (!initialized) {
+            groundPos = transform.position;
+            initialized = true;
+        }
+
         if (currentHealth < enemyData.maxHealth && !isAngry) {
             speed = speed * 1.2f;
             isAngry = true;
             animator.SetBool("isAngry", true);
         }
-        transform.position += direction * speed * Time.deltaTime;
+
+        hopTimer += Time.deltaTime;
+
+        if (isHopping) {
+            float hopProgress = hopTimer / hopDuration;
+
+            if (hopProgress >= 1f) {
+                // Once the hop is completed, enter the landing phase
+                transform.position = groundPos;
+                isHopping = false;
+                hopTimer = 0f;
+                return;
+            }
+
+            // Move ground pos along a straight path toward target so we
+            // move in an overall linear path while maintianing repeated arc path
+            Vector3 direction = (target - groundPos).normalized;
+            groundPos += direction * speed * Time.deltaTime;
+
+            // Bunny at the ground position + arc of vertical hop
+            float yOffset = Mathf.Sin(hopProgress * Mathf.PI) * hopHeight;
+            transform.position = groundPos + new Vector3(0f, yOffset, 0f);
+        } else {
+            // If we aren't mid-hop, we are in the land position
+            if (hopTimer >= landDuration) {
+                isHopping = true;
+                hopTimer = 0f;
+            }
+        }
     }
 }
